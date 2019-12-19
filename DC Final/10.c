da47b53d79f2ec3e4ab37e5e0231847e656bb39c
freak153 <38175555+freak153@users.noreply.github.com>
2019-12-19 15:30:21 +0530

    Add files via upload

---
+++ DC Final/10.c
@@ -0,0 +1,148 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <time.h>
+
+# include "mpi.h"
+
+int main ( int argc, char *argv[] );
+void timestamp ( );
+
+int main ( int argc, char *argv[] )
+{
+  MPI_Comm even_comm_id;
+  MPI_Group even_group_id;
+  int even_id;
+  int even_id_sum;
+  int even_p;
+  int *even_rank;
+  int i;
+  int id;
+  int ierr;
+  int j;
+  MPI_Comm odd_comm_id;
+  MPI_Group odd_group_id;
+  int odd_id;
+  int odd_id_sum;
+  int odd_p;
+  int *odd_rank;
+  int p;
+  MPI_Group world_group_id;
+
+  ierr = MPI_Init ( &argc, &argv );
+
+  if ( ierr != 0 )
+  {
+    printf ( "\n" );
+    printf ( "COMMUNICATOR_MPI: Fatal error!\n" );
+    printf ( "  MPI_Init returned nonzero error flag\n" );
+    exit ( 1 );
+  }
+
+  ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
+
+  ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
+
+  if ( id == 0 ) 
+  {
+    timestamp ( );
+    printf ( "\n" );
+    printf ( "COMMUNICATOR_MPI - Master process:\n" );
+    printf ( "  C/MPI version\n" );
+    printf ( "  An MPI example program.\n" );
+    printf ( "\n" );
+    printf ( "  The number of processes is %d.\n", p );
+    printf ( "\n" );
+  }
+
+  printf ( "  Process %d says 'Hello, world!'\n", id );
+
+  MPI_Comm_group ( MPI_COMM_WORLD, &world_group_id );
+
+  even_p = ( p + 1 ) / 2;
+  even_rank = ( int * ) malloc ( even_p * sizeof ( int ) );
+  j = 0;
+  for ( i = 0; i < p; i = i + 2 )
+  {
+    even_rank[j] = i;
+    j = j + 1;
+  }
+  MPI_Group_incl ( world_group_id, even_p, even_rank, &even_group_id );
+
+  MPI_Comm_create ( MPI_COMM_WORLD, even_group_id, &even_comm_id );
+
+  odd_p = p / 2;
+  odd_rank = ( int * ) malloc ( odd_p * sizeof ( int ) );
+  j = 0;
+  for ( i = 1; i < p; i = i + 2 )
+  {
+    odd_rank[j] = i;
+    j = j + 1;
+  }
+  MPI_Group_incl ( world_group_id, odd_p, odd_rank, &odd_group_id );
+
+  MPI_Comm_create ( MPI_COMM_WORLD, odd_group_id, &odd_comm_id );
+
+  if ( id % 2 == 0 )
+  {
+    ierr = MPI_Comm_rank ( even_comm_id, &even_id );
+    odd_id = -1;
+  }
+  else
+  {
+    ierr = MPI_Comm_rank ( odd_comm_id,  &odd_id );
+    even_id = -1;
+  }
+
+  if ( even_id != -1 )
+  {
+    MPI_Reduce ( &id, &even_id_sum, 1, MPI_INT, MPI_SUM, 0, even_comm_id );
+  }
+  if ( even_id == 0 )
+  {
+    printf ( "  Number of processes in even communicator = %d\n", even_p );
+    printf ( "  Sum of global ID's in even communicator  = %d\n", even_id_sum );
+  }
+
+  if ( odd_id != -1 )
+  {
+    MPI_Reduce ( &id, &odd_id_sum,  1, MPI_INT, MPI_SUM, 0, odd_comm_id );
+  }
+  if ( odd_id == 0 )
+  {
+    printf ( "  Number of processes in odd communicator  = %d\n", odd_p );
+    printf ( "  Sum of global ID's in odd communicator   = %d\n", odd_id_sum );
+  }
+
+  ierr = MPI_Finalize ( );
+
+  if ( id == 0 )
+  {
+    printf ( "\n" );
+    printf ( "COMMUNICATOR_MPI:\n" );
+    printf ( "  Normal end of execution.\n" );
+    printf ( "\n" );
+    timestamp ( );
+  }
+  return 0;
+}
+
+void timestamp ( void )
+
+
+{
+# define TIME_SIZE 40
+
+  static char time_buffer[TIME_SIZE];
+  const struct tm *tm;
+  time_t now;
+
+  now = time ( NULL );
+  tm = localtime ( &now );
+
+  strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm );
+
+  fprintf ( stdout, "%s\n", time_buffer );
+
+  return;
+# undef TIME_SIZE
+}
